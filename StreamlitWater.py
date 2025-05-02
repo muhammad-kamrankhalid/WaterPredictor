0a200bdba0d6ce7d79c33c4aa68e025acd3d2db9
muhammad-kamrankhalid <156169100+muhammad-kamrankhalid@users.noreply.github.com>
2025-05-02 10:14:59 +0500

    Update StreamlitWater.py

--- StreamlitWater.py
+++ StreamlitWater.py
@@ -1,7 +1,6 @@
 import pickle
 import streamlit as st
 import numpy as np
-import pandas as pd
 # 1. Page Setup
 st.set_page_config(page_title="💧 Full Water Potability Predictor", page_icon="💧", layout="centered")
 st.markdown(
